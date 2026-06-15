e0e610db0e8c0cd270c75761f6265901ad99d8b2
Maksym Bezuhlyi <maxim.bez.1990@gmail.com>
2026-06-15 21:18:05 +0300

    New feature a*a

--- GIT_TEST/main.cpp
+++ GIT_TEST/main.cpp
@@ -6,5 +6,12 @@ int main()
 	std::cout << "Hello git!!!\n";
 	std::cout << "This is ne project!\n";
 
+	int a = 0;
+
+	std::cout << "input a: ";
+	std::cin >> a;
+
+	std::cout << "a^2 = " << a * a << "\n";
+
 	return 0;
 }
